c9e2a579819476647d6230d1a5891057276a3cf9
Johndere <johndere54@seznam.cz>
2015-11-07 14:22:54 +0100

    Prace na funkci readHeader

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@ int main(int argc, char* argv[])
 {
 		FILE* fd = fopen("input.net", "r");
 		
+		readHeader();
 		std::vector parts = readParts(fd);
 		std::vector wires = readParts(fd);
 }
--- src/read.cpp
+++ src/read.cpp
@@ -1,6 +1,17 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <iostream>
+#include <cstring>
+
+void readHeader(FILE* fd)
+{
+	char[30] header;
+	fscanf("fd", "\"%[^\"]\"", header);
+	if(strcmp(header, "\"ExpressPCB Netlist\""))
+		throw 2;
+		
+	
+}
 
 std::vector readParts(FILE* fd)
 {
